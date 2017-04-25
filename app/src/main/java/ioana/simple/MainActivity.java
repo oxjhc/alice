81b4502771ea2c9df238f0bff038eb5e29d32888
cvlasov <catherinev.canada@gmail.com>
2017-04-25 19:19:33 +0100

    Created key pair when app is opened

--- app/src/main/java/ioana/simple/MainActivity.java
+++ app/src/main/java/ioana/simple/MainActivity.java
@@ -1,7 +1,10 @@
 package ioana.simple;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Bundle;
+import android.security.keystore.KeyProperties;
+import android.security.keystore.KeyGenParameterSpec;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -9,8 +12,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.Signature;
+import java.security.spec.ECGenParameterSpec;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String USER_KEY_NAME = "userKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +30,50 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button getPingBtn = (Button) findViewById(R.id.get_ping);
-        getPingBtn.setOnClickListener(new View.OnClickListener() {
+        checkKeyPairExists();
+
+        Button getVerifyLocationBtn = (Button) findViewById(R.id.verify_location);
+        getVerifyLocationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              getPing(view);
+              verifyLocation(view);
             }
         });
     }
 
-    public void getPing(View view) {
-        Intent intent = new Intent(this, GetPingActivity.class);
+    @TargetApi(23)
+    public void checkKeyPairExists() {
+        try {
+            // Check if public/private key pair has already been created
+            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+
+            if (keyStore.getKey(USER_KEY_NAME, null) == null) {
+                KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(
+                        KeyProperties.KEY_ALGORITHM_EC, "AndroidKeyStore");
+                keyPairGenerator.initialize(
+                        new KeyGenParameterSpec.Builder(
+                                USER_KEY_NAME,
+                                KeyProperties.PURPOSE_SIGN)
+                                .setAlgorithmParameterSpec(new ECGenParameterSpec("secp256r1"))
+                                .setDigests(KeyProperties.DIGEST_SHA256,
+                                        KeyProperties.DIGEST_SHA384,
+                                        KeyProperties.DIGEST_SHA512)
+                                // Only permit the private key to be used if the user authenticated
+                                // within the last five minutes.
+                                .setUserAuthenticationRequired(true)
+                                .setUserAuthenticationValidityDurationSeconds(5 * 60 /* seconds */)
+                                .build());
+                KeyPair keyPair = keyPairGenerator.generateKeyPair();
+                Signature signature = Signature.getInstance("SHA256withECDSA");
+                signature.initSign(keyPair.getPrivate());
+            }
+        } catch (Exception e) { /* NO EXCEPTION EXPECTED */ }
+    }
+
+    public void verifyLocation(View view) {
+        Intent intent = new Intent(this, VerifyLocationActivity.class);
+        intent.putExtra("userKeyName", USER_KEY_NAME);
         startActivity(intent);
     }
 
--- app/src/main/java/ioana/simple/VerifyLocationActivity.java
+++ app/src/main/java/ioana/simple/VerifyLocationActivity.java
@@ -2,6 +2,7 @@ package ioana.simple;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,10 +11,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
-public class GetPingActivity extends Activity {
+public class VerifyLocationActivity extends Activity {
     // Progress Dialog
     ProgressDialog progressDialog;
+    private String userKeyName;
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +28,23 @@ public class GetPingActivity extends Activity {
         setContentView(R.layout.get_ping);
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Looking for verifier...");
+
+        // Get user key name from intent
+        Intent intent = getIntent();
+        userKeyName = intent.getStringExtra("userKeyName");
+
+        try {
+            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null); // Reload Android key store
+            privateKey = (PrivateKey) keyStore.getKey(userKeyName, null);
+            publicKey = keyStore.getCertificate(userKeyName).getPublicKey();
+        } catch (Exception e) { /* NO EXCEPTION EXPECTED */ }
+
+        // Call verifyLocation(view) to initiate communication with AP
     }
 
-    public void getPing(View view) throws IOException {
+    // NOT BEING CALLED ANYWHERE YET
+    public void verifyLocation(View view) throws IOException {
         progressDialog.show();
         URL url = new URL("localhost:8080");
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -33,6 +54,4 @@ public class GetPingActivity extends Activity {
             urlConnection.disconnect();
         }
     }
-
-
 }
