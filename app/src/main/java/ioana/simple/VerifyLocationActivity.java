137f7e468c622fcf0edab004a52ca0013719dbdb
cvlasov <catherinev.canada@gmail.com>
2017-04-30 23:52:50 +0100

    Made communication with the AP work!!

--- app/src/main/java/ioana/simple/VerifyLocationActivity.java
+++ app/src/main/java/ioana/simple/VerifyLocationActivity.java
@@ -2,22 +2,32 @@ package ioana.simple;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
+import android.net.wifi.p2p.WifiP2pManager.DnsSdTxtRecordListener;
+import android.net.wifi.p2p.WifiP2pManager.DnsSdServiceResponseListener;
+import android.net.wifi.p2p.WifiP2pManager.UpnpServiceResponseListener;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
+import android.net.wifi.p2p.nsd.WifiP2pUpnpServiceRequest;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.protobuf.ByteString;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.HttpURLConnection;
-import java.net.Socket;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.security.KeyPair;
@@ -25,16 +35,30 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class VerifyLocationActivity extends Activity {
-    // Progress Dialog
+    public static final String TAG = "VerifyLocationActivity";
+
     ProgressDialog progressDialog;
     private final int AP_PORT = 1832;
     private String userKeyName;
     private KeyPair keyPair;
     private PublicKey publicKey;
     private PrivateKey privateKey;
+
+    private NsdManager.DiscoveryListener discoveryListener;
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private WiFiDirectBroadcastReceiver receiver;
+    private final IntentFilter intentFilter = new IntentFilter();
+    private boolean isWifiP2pEnabled = false;
+    private boolean retryChannel = false;
+
+    final HashMap<String, String> nearbyAPs = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,13 +78,199 @@ public class VerifyLocationActivity extends Activity {
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage(getResources().getString(R.string.progress_msg));
 
-        try {
-            verifyLocation();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+
+        manager.createGroup(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.d(TAG, "Successfully created group");
+            }
+
+            @Override
+            public void onFailure(int code) {
+                // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+                Log.d(TAG, "Failed to create group (code: " + code + ")");
+            }
+        });
+
+        discoverService(); // add Upnp service response listener to manager
+
+        WifiP2pUpnpServiceRequest serviceRequest = WifiP2pUpnpServiceRequest.newInstance("urn:schemas-oxjhc-club:service:Rabbits:1");
+        manager.addServiceRequest(channel,
+                serviceRequest,
+                new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d(TAG, "Successfully added request");
+                    }
+
+                    @Override
+                    public void onFailure(int code) {
+                        // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+                        Log.d(TAG, "Failed to add request");
+                    }
+                });
+
+                manager.discoverServices(channel, new ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d(TAG, "Successfully discovered services");
+                    }
+
+                    @Override
+                    public void onFailure(int code) {
+                        // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+                        Log.d(TAG, "Unsuccessfully discovered services");
+
+                        if (code == WifiP2pManager.P2P_UNSUPPORTED) {
+                            Log.d(TAG, "P2P isn't supported on this device.");
+                        } // else if (...) ...
+                    }
+                });
+
+
+        // AFTER GROUP CREATE AND SERVICE DISCOVERY FINISH, THEN PEER CONNECT :)
+
+
+//        WifiP2pDnsSdServiceRequest serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
+//        manager.addServiceRequest(channel,
+//                serviceRequest,
+//                new WifiP2pManager.ActionListener() {
+//                    @Override
+//                    public void onSuccess() {
+//                        Log.d(TAG, "Request successfully added!");
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int code) {
+//                        // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+//                        Log.d(TAG, "Action failed :(");
+//                    }
+//                });
+//
+
+
+//        try {
+//            verifyLocation();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    private void discoverService() {
+//        DnsSdTxtRecordListener txtListener = new DnsSdTxtRecordListener() {
+//
+//            @Override
+//            /* Callback includes:
+//             * fullDomain: full domain name: e.g "printer._ipp._tcp.local."
+//             * record: TXT record dta as a map of key/value pairs.
+//             * device: The device running the advertised service.
+//             */
+//            public void onDnsSdTxtRecordAvailable(
+//                    String fullDomain, Map<String,String> record, WifiP2pDevice device) {
+//                Log.d(TAG, "DnsSdTxtRecord available -" + record.toString());
+//                nearbyAPs.put(device.deviceAddress, record.get("AP_NAME_HERE"));
+//            }
+//        };
+
+//        DnsSdServiceResponseListener servListener = new DnsSdServiceResponseListener() {
+//            @Override
+//            public void onDnsSdServiceAvailable(String instanceName, String registrationType,
+//                                                WifiP2pDevice resourceType) {
+//
+//                // Update the device name with the human-friendly version from
+//                // the DnsTxtRecord, assuming one arrived.
+//                resourceType.deviceName = nearbyAPs
+//                        .containsKey(resourceType.deviceAddress) ? nearbyAPs
+//                        .get(resourceType.deviceAddress) : resourceType.deviceName;
+//
+//                // Add to the custom adapter defined specifically for showing
+//                // wifi devices.
+//                /* WiFiDirectServicesList fragment = (WiFiDirectServicesList) getFragmentManager()
+//                        .findFragmentById(R.id.frag_peerlist);
+//                WiFiDevicesAdapter adapter = ((WiFiDevicesAdapter) fragment
+//                        .getListAdapter());
+//
+//                adapter.add(resourceType);
+//                adapter.notifyDataSetChanged();
+//                Log.d(TAG, "onBonjourServiceAvailable " + instanceName); */
+//            }
+//        };
+
+//        manager.setDnsSdResponseListeners(channel, servListener, txtListener);
+
+
+
+        UpnpServiceResponseListener upnpListener = new UpnpServiceResponseListener() {
+            @Override
+            public void onUpnpServiceAvailable(List<String> uniqueServiceNames, WifiP2pDevice srcDevice) {
+                    // check that it is of the form "urn:schemas-oxjhc-club:service:Rabbits:1"
+                    if (uniqueServiceNames.size() > 0) {
+                        for (String name : uniqueServiceNames) {
+                            Log.d(TAG, "service name: " + name);
+                        }
+                        Log.d(TAG, "Found Sauyon!");
+                        clearServiceRequests();
+                    } else {
+                        Log.d(TAG, "Couldn't find Sauyon :(");
+                    }
+            }
+        };
+
+        manager.setUpnpServiceResponseListener(channel, upnpListener);
+    }
+
+    public void clearServiceRequests() {
+        manager.clearServiceRequests(channel, new ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.d(TAG, "Cleared service requests");
+            }
+
+            @Override
+            public void onFailure(int code) {
+                Log.d(TAG, "Failed to clear service requests");
+            }
+        });
+    }
+
+    /**
+     * Remove all peers and clear all fields. This is called on
+     * BroadcastReceiver receiving a state change event.
+     */
+    public void resetData() {
+        /*DeviceListFragment fragmentList = (DeviceListFragment) getFragmentManager()
+                .findFragmentById(R.id.frag_list);
+        DeviceDetailFragment fragmentDetails = (DeviceDetailFragment) getFragmentManager()
+                .findFragmentById(R.id.frag_detail);
+        if (fragmentList != null) {
+            fragmentList.clearPeers();
+        }
+        if (fragmentDetails != null) {
+            fragmentDetails.resetViews();
+        }*/
+    }
+
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
+
+
 
     public void verifyLocation() throws IOException {
         progressDialog.show();
@@ -72,7 +282,7 @@ public class VerifyLocationActivity extends Activity {
             urlConnection.disconnect();
         }*/
 
-        DatagramSocket datagramSocket = new DatagramSocket(AP_PORT);
+        /*DatagramSocket datagramSocket = new DatagramSocket(AP_PORT);
         URL url = new URL("http://" + datagramSocket.getInetAddress().getHostAddress());
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         InputStream fromAP = new BufferedInputStream(connection.getInputStream());
@@ -80,11 +290,11 @@ public class VerifyLocationActivity extends Activity {
 
         int seqid = receiveSeqId(datagramSocket);
         sendProofReq(toAP, seqid);
-        receiveProofResp(fromAP);
+        receiveProofResp(fromAP);*/
 
         // DO OTHER COMMUNICATIONS
 
-        connection.disconnect();
+        //connection.disconnect();
     }
 
     /** Return sequence id, which is received from the AP */
